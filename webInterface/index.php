497c5cd11b65ec836896c7b2dd86360d287dd33f
ducdinh-codehub <dinhduc31197@gmail.com>
2022-05-16 22:44:34 +0700

    Fixing saving feature

--- webInterface/index.php
+++ webInterface/index.php
@@ -1,6 +1,8 @@
 <html>
     <head>
         <script type="text/javascript" src="./jquery-3.6.0.min.js"></script>
+        <script type="text/javascript" src="./html2canvas.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js"></script>
         <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
         <style>
             body{
@@ -43,6 +45,7 @@
 
             $("#menuItem2").click(function (){
                 $("#screen").css("visibility", "visible");
+                $("#closeBtb").css("visibility", "visible");
                 idInterval = window.setInterval(function(){
 				    getFiles();
 			    }, 1000); 
@@ -52,8 +55,20 @@
             $("#backBtb").click(function (){
                 idInterval = window.setInterval(function(){
 				    getFiles();
+                    $("#saveBtb").css("visibility","hidden");
+                    $("#backBtb").css("visibility","hidden");
 			    }, 1000); 
+                
             });
+
+            $("#saveBtb").click(function (){
+                    html2canvas(document.querySelector("#content")).then(canvas => {
+                        document.body.appendChild(canvas)
+                        //canvas.toBlob(function(blob) {
+                        //    window.saveAs(blob, 'my_image.jpg');
+                    //});
+                });
+            }); 
 
             function getFiles(){
                 $.ajax({
@@ -82,14 +97,19 @@
                     cache: false,
                 }).done(function(html){
                     clearInterval(idInterval);
+                    $("#closeBtb").css("visibility","visible");
+                    $("#saveBtb").css("visibility","visible");
                     $("#backBtb").css("visibility","visible");
                     $("#display").html(html);
                 })
             });
             
             $("#closeBtb").click(function (){
+                $("#saveBtb").css("visibility","hidden");
                 $("#screen").css("visibility","hidden");
                 $("#backBtb").css("visibility","hidden");
+                $("#closeBtb").css("visibility","hidden");
+                //$("#display").val("");
                 clearInterval(idInterval);
             });
             $("#menuItem1").click(function (){
@@ -116,8 +136,9 @@
 
         <div id="screen" style="margin: 2% auto; width: 70%; height:70%; border: 3px solid green;">
             <div id="display" style="width: 100%; height: 95%;"></div>
-            <div id='closeBtb' style='width: 20%; height: 5%; background-color: gray; color: white; float: right; text-align: center;'>Close</div>
-            <div id='backBtb' style='width: 20%; height: 5%; background-color: gray; color: white; float: right; text-align: center; margin-right: 2%; visibility: hidden'>Back</div>
+            <div id='closeBtb' style='width: 10%; height: 5%; background-color: gray; color: white; float: right; text-align: center;'>Close</div>
+            <div id='backBtb' style='width: 10%; height: 5%; background-color: gray; color: white; float: right; text-align: center; margin-right: 1%; visibility: hidden'>Back</div>
+            <div id='saveBtb' style='width: 10%; height: 5%; background-color: gray; color: white; float: right; text-align: center; margin-right: 1%; visibility: hidden'>Save</div>
         </div>
     </body>
     
